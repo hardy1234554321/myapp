cd9c075ae811a459fd3e8dc65c08b1ec70579d6a
shock_hsu <shock_hsu@silicon-power.com>
2022-04-17 04:06:18 +0800

    更新api判斷

---
+++ lib/loginView.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isEnabled = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final url = 'https://myapi-auth.herokuapp.com/api/post/auth';
+
+  Future<void> singinEvent() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isEnabled = !_isEnabled;
+      });
+
+      if (!_isEnabled) {
+        final response = await http.post(
+          Uri.parse(url),
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: jsonEncode({
+            'username': _usernameController.text,
+            'password': _passwordController.text
+          }),
+        );
+
+        if (response.statusCode == 200) {
+          var responseData = jsonDecode(response.body);
+
+          //顯示下方提示訊息
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                  '${_usernameController.text}, login ${responseData['status']}!'),
+            ),
+          );
+        } else {
+          throw Exception('Failed to auth.');
+        }
+      }
+    }
+  }
+
+  void singupEvent() {
+    //顯示跳出視窗
+    if (_formKey.currentState!.validate()) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text('Welcome, ${_usernameController.text}'),
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign In',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    enabled: _isEnabled,
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Username is required!';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    enabled: _isEnabled,
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required!';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextButton(
+                    onPressed: singinEvent,
+                    child: _isEnabled
+                        ? const Text('Sign in')
+                        : const Text('Waiting..'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      minimumSize: const Size.fromHeight(50),
+                      textStyle: const TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      backgroundColor: Colors.blue[50],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    onPressed: singupEvent,
+                    child: const Text('Sign up'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      minimumSize: const Size.fromHeight(50),
+                      textStyle: const TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      backgroundColor: Colors.blue[300],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'loginView.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,114 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Demo'),
-        ),
-        body: formSection(),
-      ),
-    );
-  }
-}
-
-class formSection extends StatefulWidget {
-  @override
-  State<formSection> createState() => formSectionState();
-}
-
-class formSectionState extends State<formSection> {
-  bool _isEnabled = true;
-
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController loginController = TextEditingController();
-
-  void buttonClicksEvent() {
-    setState(() {
-      _isEnabled = !_isEnabled;
-    });
-
-    if (!_isEnabled) {
-      showAlertDialog(context);
-    }
-  }
-
-  void showAlertDialog(context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Text('Welcome, ${usernameController.text}'),
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12.0))),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          child: TextField(
-            enabled: _isEnabled,
-            controller: usernameController,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Username'),
-          ),
-          margin: const EdgeInsets.only(left: 5.0, right: 5.0, top: 30.0),
-        ),
-        Container(
-          child: TextField(
-            enabled: _isEnabled,
-            obscureText: true,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Password'),
-          ),
-          margin: const EdgeInsets.only(
-              left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
-        ),
-        Container(
-          margin: const EdgeInsets.only(left: 5.0, right: 5.0),
-          child: TextButton(
-            onPressed: buttonClicksEvent,
-            child: _isEnabled ? const Text('Log in') : const Text('Waiting..'),
-            style: TextButton.styleFrom(
-              primary: Colors.black,
-              minimumSize: const Size.fromHeight(50),
-              textStyle: const TextStyle(
-                fontSize: 20.0,
-              ),
-              backgroundColor: Colors.blue[50],
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.only(left: 5.0, right: 5.0),
-          child: TextButton(
-            onPressed: () {
-              showAlertDialog(context);
-            },
-            child: const Text('Sign up'),
-            style: TextButton.styleFrom(
-              primary: Colors.white,
-              minimumSize: const Size.fromHeight(50),
-              textStyle: const TextStyle(
-                fontSize: 20.0,
-              ),
-              backgroundColor: Colors.blue[300],
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-            ),
-          ),
-        ),
-      ],
+      home: LoginPage(),
     );
   }
 }
